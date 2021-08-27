95b6fc611c56e414b73c9514056b2df69db12ef4
Jopper King <jopperdeveloper@gmail.com>
2021-08-27 19:21:20 +0430

    Dataset loading

---
+++ Linear_Regression.py
@@ -0,0 +1,12 @@
+import numpy as np 
+from sklearn import linear_model
+from sklearn import datasets
+
+#dataset loading
+
+boston = datasets.load_boston() 
+
+#model learning
+
+
+#model prediction
