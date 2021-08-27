9e16d99c012e153e05388880ea76a896b1af581f
Jopper King <jopperdeveloper@gmail.com>
2021-08-27 19:54:15 +0430

    Model Learning and prediction

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -5,8 +5,18 @@ from sklearn import datasets
 #dataset loading
 
 boston = datasets.load_boston() 
+print(boston)
 
 #model learning
 
+reg = linear_model.LinearRegression()
+print(reg)
+
+reg.fit(boston.data,boston.target)
+print(reg)
 
 #model prediction
+
+result= reg.predict([boston.data[-1]])
+print(result)
+print(boston.target[-1])
